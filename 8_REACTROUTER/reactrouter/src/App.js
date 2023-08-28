e5974ea2021450a758a20934e9d13df02b089750
pedroh-leite <pedrohleite.alves@gmail.com>
2023-08-28 15:08:12 -0300

    Update

--- 8_REACTROUTER/reactrouter/src/App.js
+++ 8_REACTROUTER/reactrouter/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 // 1 - Config react routes
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}>
+          <Route path='/' element={<Home />}> </Route>
+          <Route path='/about' element={<About />}> </Route>
         </Routes>
       </BrowserRouter>
     </div>
