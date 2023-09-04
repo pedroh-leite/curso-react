d81a8687ab9a46e27afebdd21e6ec8681678166b
pedroh-leite <pedrohleite.alves@gmail.com>
2023-09-04 16:06:14 -0300

    Criando a base do projeto

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -5,17 +5,26 @@ import{BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 // pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter> 
+        <NavBar/>
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/about' element={<About />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register />}/>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ 10_MINIBLOG/miniblog/src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import styles from "./Login.modules.css";
+
+const Login = () => {
+  return (
+    <div>
+        <h2>Login</h2>
+    </div>
+  )
+}
+
+export default Login
